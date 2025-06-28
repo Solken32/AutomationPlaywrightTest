925a6402100c56a55632bbe6a16e48e2dd3e1233
solken32 <solistoke@gmail.com>
2025-06-28 10:43:05 -0500

    2. adecuando a Page objects model y aserciones(verificaciones)

--- tests/carrito_compra.spec.ts
+++ tests/carrito_compra.spec.ts
@@ -1,12 +1,18 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from './pageobjects/LoginPage';
 
 test('funcionalidad carrito compras' , async({ page }) => {
     //1. Acceder a la pagina de compras
     await page.goto('https://www.saucedemo.com/');
     //2. Ingresar credenciales e iniciar sesión
-    await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
+    /* await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
     await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByRole('button', { name: 'Login' }).click(); */
+
+    const adminUser = new LoginPage(page);
+    await adminUser.fillUsername('standard_user');
+    await adminUser.fillPassword('secret_sauce');
+    await adminUser.clickLogin();
 
     // 3. Obtener data(producto) de forma random    
     const itemsContainer = await  page.locator('div.inventory_item').all();
@@ -26,7 +32,7 @@ test('funcionalidad carrito compras' , async({ page }) => {
     // 5. Ir al carrito
     await page.locator('a.shopping_cart_link').click();
     
-    expect(page.getByRole('button', {name : 'Checkout'})).toBeVisible(); // confirmar que sea visible
+    await expect(page.getByRole('button', {name : 'Checkout'})).toBeVisible(); // confirmar que sea visible
 
     // info de carrito actual
     const nameActual= await page.locator('div.inventory_item_name').innerText();
@@ -38,7 +44,49 @@ test('funcionalidad carrito compras' , async({ page }) => {
     expect(descriptionActual).toEqual(descriptionProduct);
     expect(priceActual).toEqual(priceProduct);
 
+    // 5. Proceder al pago
+    await page.getByRole('button', { name: 'Checkout'}).click();
+
+    await page.locator('input#first-name').fill('Kenyo');
+    await page.locator('input#last-name').fill('Solis');
+    await page.locator('input#postal-code').fill('17513');
+
+    await page.getByRole('button', {name: 'Continue'}).click();
+    await page.getByRole('button', {name: 'Finish'}).click();
+
+    await expect(page.locator('h2.complete-header')).toBeVisible();
+
     await page.pause();
 
+
+})
+
+
+test('Login' , async({ page }) => {
+    //1. Acceder a la pagina de compras
+    await page.goto('https://www.saucedemo.com/');
+    //2. Ingresar credenciales e iniciar sesión
+
+    const adminUser = new LoginPage(page);
+    await adminUser.fillUsername('locked_out_user');
+    await adminUser.fillPassword('secret_sauce');
+    await adminUser.clickLogin();
+    
+    await page.pause();
+
+
+})
+
+test('Login option2' , async({ page }) => {
+    //1. Acceder a la pagina de compras
+    await page.goto('https://www.saucedemo.com/');
+    //2. Ingresar credenciales e iniciar sesión
+    const login = new LoginPage(page);
+    await login.loginWhitCredentials('standard_user','secret_sauce')
+
+    //3. confirmar que se hizo login
+    await login.checkSuccessfullLogin();
+    
+    await page.pause();
 
 })
---
+++ tests/pageobjects/LoginPage.ts
@@ -0,0 +1,39 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export class LoginPage {
+
+    private readonly usernameTextbox: Locator
+    private readonly passwordTextbox: Locator
+    private readonly loginButton: Locator
+    private readonly shoppingCartIcon: Locator
+
+    constructor(page : Page){
+        this.usernameTextbox = page.getByRole('textbox', { name: 'Username' });
+        this.passwordTextbox = page.getByRole('textbox', { name: 'Password' });
+        this.loginButton = page.getByRole('button', { name: 'Login' });
+        this.shoppingCartIcon = page.locator("xpath=//a[contains(@class,'shopping_cart_link')]");
+    }
+
+    async fillUsername(username:string){
+        await this.usernameTextbox.fill(username);
+    }
+
+    async fillPassword(password : string){
+        await this.passwordTextbox.fill(password);
+    }
+
+    async clickLogin(){
+        await this.loginButton.click();
+    }
+
+    async loginWhitCredentials(username: string , password: string){
+        await this.fillUsername(username);
+        await this.fillPassword(password);
+        await this.clickLogin();
+    }
+
+    async checkSuccessfullLogin(){
+        await expect(this.shoppingCartIcon).toBeVisible();
+    }
+
+}
